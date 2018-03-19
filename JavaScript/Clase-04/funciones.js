9d04f43725943e5e2a73025dd79c1b3008577d4c
Christopher Velázquez <christo_9722@hotmail.com>
2018-03-19 00:11:45 -0600

    Intro a Funciones

--- JavaScript/Clase-04/funciones.js
+++ JavaScript/Clase-04/funciones.js
@@ -20,7 +20,8 @@ function sumar(a,b){
 let resultado = sumar(6,5);
 console.log(resultado);
 
-// cuando una funcion esta den
+// cuando una funcion esta dentro de un objeto ya no se llama funcion si no que ahora es un metodo
+// sumar es un metodo del objeto 
 let obj = {
     nombre: 'objeto',
     sumar(a,b) {
@@ -28,6 +29,20 @@ let obj = {
     }
 };
 
+let miOtraSuma = obj.sumar(7,8);
+console.log(`El resultado es: ${miOtraSuma}`);
+
+//-----------------FUNCIONES DENTRO DE OTRAS FUNCIONES-----------
+
+function restar(a){
+    return function(b){
+        return a - b
+    }
+}
+//con el segundo parentesis ando indicando que también voy a ejecutar la segunda función ya que en la primera
+// cuando encuentra el return hasta ahí se acaba junto con el primer parantesis 
+let miResta = restar(10)(3);
+console.log(miResta);
 
 
 
